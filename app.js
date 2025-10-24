b03b039004ddfb0e907088e47ae87b7593f12096
borgesvagnervbs-ux <borges.vagnervbs@gmail.com>
2025-10-24 20:30:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const DB_NAME = 'LembretesDB';
 const STORE_NAME = 'meds';
 let db = null;
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registrado.'))
+    .catch(err => console.error('Erro ao registrar o SW:', err));
+}
+
+
 function openDB() {
   return new Promise((resolve, reject) => {
     if (db) return resolve(db);
